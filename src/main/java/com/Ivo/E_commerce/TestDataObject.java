52572185b7cb6b0fe5064bb7473144c61cdce18f
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-08-26 23:12:38 +0700

    Ivo

--- src/main/java/com/Ivo/E_commerce/TestDataObject.java
+++ src/main/java/com/Ivo/E_commerce/TestDataObject.java
@@ -1,4 +1,15 @@
 package com.Ivo.E_commerce;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class TestDataObject {
+
+    @Id
+    private long id;
+    @Column
+    private String userName;
+
 }
